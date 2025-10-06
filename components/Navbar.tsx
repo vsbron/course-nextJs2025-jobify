627bd305bbb67ac9d64e1807d8edc921ba69b3e6
vsbron <vsbron@gmail.com>
2025-10-06 14:21:53 +0300

    Set up the Sidebar and Navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+
+import LinksDropdown from "./LinksDropdown";
+import ThemeToggle from "./ThemeToggle";
+
 function Navbar() {
   // Returned JSX
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+      <LinksDropdown />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,37 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { Button } from "./ui/button";
+
+import links from "@/utils/links";
+import Logo from "@/assets/logo.svg";
+
 function Sidebar() {
+  // Get the current path
+  const pathname = usePathname();
+
   // Returned JSX
-  return <div>Sidebar</div>;
+  return (
+    <aside className="h-full py-4 px-8 bg-muted">
+      <Image src={Logo} alt="Jobify" className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map(({ href, label, icon }) => (
+          <Button
+            key={href}
+            variant={pathname === href ? "default" : "link"}
+            asChild
+          >
+            <Link href={href} className="flex items-center gap-x-2">
+              {icon}
+              <span className="capitalize">{label}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
