23d6f9895f78f44531b6c1b8b2f2117fd7130cd8
vsbron <vsbron@gmail.com>
2025-10-07 13:07:46 +0300

    Set up the Links for the dropdown component

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,6 +1,41 @@
+import Link from "next/link";
+import { AlignLeft } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import links from "@/utils/links";
+
 function LinksDropdown() {
   // Returned JSX
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="lg:hidden" asChild>
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map(({ href, label, icon }) => (
+          <DropdownMenuItem key={href} asChild>
+            <Link href={href} className="flex items-center gap-x-2">
+              {icon}
+              <span className="capitalize">{label}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,9 @@ function Navbar() {
   // Returned JSX
   return (
     <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
-      <LinksDropdown />
+      <div>
+        <LinksDropdown />
+      </div>
       <div className="flex items-center gap-x-4">
         <ThemeToggle />
         <UserButton afterSignOutUrl="/" />
